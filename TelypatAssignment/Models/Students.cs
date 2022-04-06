6648c93036b1cdcc92c3560ce006c3668e490edf
saharalhabsi <sahar96alhabsi@gmail.com>
2022-04-06 22:56:15 +0400

    Update Students.cs

--- TelypatAssignment/Models/Students.cs
+++ TelypatAssignment/Models/Students.cs
@@ -13,8 +13,11 @@ namespace TelypayAssignment.Models
         public string classes { get; set; }
         public string country { get; set; }
         public string birthDate { get; set; }
-        /*        public string students_id { get; set; }
-               public string total { get; set; }*/
+     /*   public int stdage
+        {
+            get;
+            set;
 
+        }*/
     }
 }
